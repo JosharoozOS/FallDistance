312e19e9b8bab708964fa6b0b032a5f042847801
JosharoozOS <heinjos@oregonstate.edu>
2026-04-21 14:54:32 -0700

    Update fall_distance.py

--- fall_distance.py
+++ fall_distance.py
@@ -3,10 +3,12 @@
 # Date: 4/21/2026
 # Description: create a program that returns the distance that an object falls when given an amount of time.
 
+g = 9.8
+
 def fall_distance(t):
 
-    g = 9.8
+
     d = (1 / 2) * g * t ** 2
-    
+
 
     return d #returns the fall distance
